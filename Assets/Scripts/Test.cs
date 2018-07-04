660259b831e90d0516495397c1c7e470d552f809
Will <tangwilliam@126.com>
2018-07-04 17:28:57 +0800

    Initial commit

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        GameObject a = new GameObject();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
