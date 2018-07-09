e007653e222e9041ee3c8edd62122d3520a0b487
唐秦崴 <tangqinwei@corp.netease.com>
2018-07-09 18:44:25 +0800

    Cam position with player move

---
+++ Assets/Scripts/CamMotor.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMotor : MonoBehaviour {
+
+    public float heightOffset = 3.5f;
+    public float behindOffset = 5.0f;
+    public float smoothSpeed = 5.0f;
+
+    public Transform targetTrans;
+
+
+    private Vector3 offset;
+    private Vector3 desiredPosition;
+
+    private void Start()
+    {
+        offset = new Vector3(0, heightOffset, -1 * behindOffset);
+    }
+
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            RotateOffsetVector(true);
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+            RotateOffsetVector(false);
+        
+    }
+
+    private void FixedUpdate()
+    {
+        desiredPosition = targetTrans.position + offset;
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+
+        transform.LookAt(targetTrans.position + Vector3.up);
+
+    }
+
+
+    private void RotateOffsetVector( bool left )
+    {
+        float rotateSign;
+        if (left)
+            rotateSign = -1.0f;
+        else
+            rotateSign = 1.0f;
+
+        offset = Quaternion.Euler(0, rotateSign * 90, 0) * offset;
+
+    }
+
+}
---
+++ Assets/Scripts/Motor.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Motor : MonoBehaviour {
+
+    public float moveSpeed = 1.0f;
+    public float drag = 1.0f;
+    public float terminalRotationSpeed = 35.0f;
+
+
+    private Rigidbody rb;
+
+    private Transform camTrans;
+
+	// Use this for initialization
+	void Start () {
+
+        rb = GetComponent<Rigidbody>();
+        rb.drag = drag;
+        rb.maxAngularVelocity = terminalRotationSpeed;
+
+        camTrans = Camera.main.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 dir = Vector3.zero;
+
+        dir.x = Input.GetAxis("Horizontal");
+        dir.z = Input.GetAxis("Vertical");
+
+        if( dir.magnitude > 1 )
+        {
+            dir.Normalize();
+        }
+
+        dir = camTrans.forward * dir.magnitude;
+
+        rb.AddForce(dir * moveSpeed);
+
+
+
+	}
+}
